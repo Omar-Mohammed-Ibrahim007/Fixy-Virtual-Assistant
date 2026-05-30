3a24b46417a17182cd31154160bf9823e5d95657
Omar Mohammed Ibrahim Ali <omar.omaralasheb@gmail.com>
2026-05-30 21:56:25 +0300

    in llm.py add force_download

--- app/llm.py
+++ app/llm.py
@@ -1,9 +1,8 @@
 from huggingface_hub import hf_hub_download
 from app.constants import MODEL_REPO, MODEL_FILE
 
-
 model_path = hf_hub_download(
     repo_id=MODEL_REPO,
     filename=MODEL_FILE,
+    force_download=True
 )
-
