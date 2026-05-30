0f724e0c25ccd9e6b29af8b9a30d67a708325123
Omar Mohammed Ibrahim Ali <omar.omaralasheb@gmail.com>
2026-05-30 22:35:54 +0300

    change llama version and quaniztion of llm

--- app/llm.py
+++ app/llm.py
@@ -3,7 +3,7 @@ from app.constants import MODEL_REPO, MODEL_FILE
 
 model_path = hf_hub_download(
     repo_id="Qwen/Qwen3-4B-GGUF",
-    filename="Qwen3-4B-Q6_K.gguf",
+    filename="Qwen3-4B-Q8_0.gguf",
     force_download=True,
     resume_download=False
 )
